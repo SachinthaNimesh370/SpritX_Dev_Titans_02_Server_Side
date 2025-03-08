2bd14f5653abf1618d45ec0b2526c7020f3bb96e
sachinthanimesh370 <sachinthanimesh370@gmail.com>
2025-03-08 23:24:14 +0530

    Create Template for spritx part 2

---
+++ src/main/java/com/mora/SpiritX_Dev_Titans_02_Server_Side/SpiritXDevTitans02ServerSideApplication.java
@@ -0,0 +1,13 @@
+package com.mora.SpiritX_Dev_Titans_02_Server_Side;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiritXDevTitans02ServerSideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpiritXDevTitans02ServerSideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mora/SpiritX_Dev_Titans_02_Server_Side/SpiritXDevTitans02ServerSideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mora.SpiritX_Dev_Titans_02_Server_Side;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiritXDevTitans02ServerSideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
